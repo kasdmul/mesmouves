b27f649e69c4457f5f28686a9a5afbd3bf3c8a2c
Serge KABEYA <kasdmul@gmail.com>
2025-06-20 22:08:37 +0000

    efface tout l'application on recommence tout à 0

--- src/components/layout/app-provider.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { AuthProvider } from '@/context/auth-context';
-
-export function AppProvider({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
