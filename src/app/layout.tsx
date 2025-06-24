2873cf128111c1574201f7de660b8ec33a78a1af
Serge KABEYA <kasdmul@gmail.com>
2025-06-24 14:20:43 +0000

    Change app title to SKFUTUR Carrière

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "RH Insights",
+  title: "SKFUTUR Carrière",
   description: "Tableau de Bord RH pour l'analytique de recrutement",
 };
 
